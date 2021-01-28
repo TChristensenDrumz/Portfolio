743c5a192d3d7fead17ae13fc66a6c5b7148c58b
TChristensenDrumz <tchristensendrumz@gmail.com>
2021-01-28 15:00:32 -0800

    updated portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer"
 
+import HARMony from "../assets/harmonythegame.gif";
+
 function Portfolio() {
     return (
         <>
@@ -11,27 +13,25 @@ function Portfolio() {
                         <div className="col-sm-9 col-xs-12">
                         <h1>Resume</h1>
                         <hr/>
-                        <a href="https://drive.google.com/uc?id=10qREGuXhuBdNJTVxIIy1C_k4siVmidPV&export=download">Download PDF</a>
+                        <a href="https://drive.google.com/uc?id=1W3Be9OTF7-eNMKt1TvDaFjmPh-FFDP2j&export=download">Download PDF</a>
                         </div>
                     </div>
                     <div className="row mt-5">
-                            <div className="col-sm-9 col-xs-12">
-                                <h1>Projects</h1>
-                                <hr/>
-                                <div>
-                                    <h3 className="pl-5">Home Cooked Comfort</h3>
-                                    <img src="https://github.com/tchristensendrumz/Home-Cooked-Comfort/raw/main/assets/screencap.gif?raw=true" className="img-fluid float-left pl-5" alt="Home-Cooked-Comfort"/>
-                                </div>
+                        <div className="col-sm-9 col-xs-12">
+                            <div>
+                                <h3 className="pl-5">HARMony</h3>
+                                <img src={HARMony} className="img-fluid float-left pl-5" alt="HARMony"/>
                             </div>
                         </div>
-                        <div className="row mt-5">
+                    </div>
+                    <div className="row mt-5">
                         <div className="col-sm-9 col-xs-12">
-                            <a className="pl-5" href="https://tchristensendrumz.github.io/Home-Cooked-Comfort/">https://tchristensendrumz.github.io/Home-Cooked-Comfort/</a>
+                            <a className="pl-5" href="https://github.com/TChristensenDrumz/HARMony">https://github.com/TChristensenDrumz/HARMony</a>
                             <br/>
-                            <a className="pl-5" href="https://github.com/TChristensenDrumz/Home-Cooked-Comfort">https://github.com/TChristensenDrumz/Home-Cooked-Comfort</a>
+                            <a className="pl-5" href="https://harmonythegame.herokuapp.com/">https://harmonythegame.herokuapp.com/</a>
                         </div>
-                        </div>
-                        <div className="row mt-5">
+                    </div>
+                    <div className="row mt-5">
                         <div className="col-sm-9 col-xs-12">
                             <div>
                                 <h3 className="pl-5">NoGym</h3>
@@ -41,11 +41,28 @@ function Portfolio() {
                     </div>
                     <div className="row mt-5">
                         <div className="col-sm-9 col-xs-12">
-                        <a className="pl-5" href="https://github.com/TChristensenDrumz/NoGym">https://github.com/TChristensenDrumz/NoGym</a>
-                        <br/>
-                        <a className="pl-5" href="https://nogym.herokuapp.com">https://nogym.herokuapp.com</a>
+                            <a className="pl-5" href="https://github.com/TChristensenDrumz/NoGym">https://github.com/TChristensenDrumz/NoGym</a>
+                            <br/>
+                            <a className="pl-5" href="https://nogym.herokuapp.com">https://nogym.herokuapp.com</a>
                         </div>
                     </div>
+                    <div className="row mt-5">
+                        <div className="col-sm-9 col-xs-12">
+                            <h1>Projects</h1>
+                            <hr/>
+                            <div>
+                                <h3 className="pl-5">Home Cooked Comfort</h3>
+                                <img src="https://github.com/tchristensendrumz/Home-Cooked-Comfort/raw/main/assets/screencap.gif?raw=true" className="img-fluid float-left pl-5" alt="Home-Cooked-Comfort"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row mt-5">
+                        <div className="col-sm-9 col-xs-12">
+                            <a className="pl-5" href="https://tchristensendrumz.github.io/Home-Cooked-Comfort/">https://tchristensendrumz.github.io/Home-Cooked-Comfort/</a>
+                            <br/>
+                            <a className="pl-5" href="https://github.com/TChristensenDrumz/Home-Cooked-Comfort">https://github.com/TChristensenDrumz/Home-Cooked-Comfort</a>
+                        </div>
+                    </div>                   
                     <div className="filler"></div>
                 </div>
             <Footer />
