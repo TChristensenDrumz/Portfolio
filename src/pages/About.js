98d17e7b6c1d4c6a8e349b6791c816d972429404
TChristensenDrumz <tchristensendrumz@gmail.com>
2021-01-28 18:29:53 -0800

    updated about me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,7 +12,7 @@ function About() {
                             <h1>About Me</h1>
                             <hr/>
                             <div className="float-left pr-3">
-                                <img src="https://ca.slack-edge.com/T01C8L6GK9Q-U01CWJ19YBT-f614b873e04d-512" className="img-fluid about" alt=""/>
+                                <img src="https://ca.slack-edge.com/T01C8L6GK9Q-U01CWJ19YBT-f614b873e04d-512" className="img-fluid about" alt="terrapinchristensen"/>
                             </div>
                             <div>
                                 <p>
@@ -20,19 +20,19 @@ function About() {
                                     currently based in Glendale, California. Terrapin has preformed with
                                     groups including the Colburn Adult Jazz Combo. Terrapin is trained in 
                                     a wide range of musical genres including jazz, rock, funk, latin, hip-hop, 
-                                    reggae, metal, and pop. Terrapin also has a year of experience in JavaScript
-                                    and is an alumni of the UCLA Extension Fullstack Coding Bootcamp.
+                                    reggae, metal, and pop. Terrapin is known to be a hard working and collaborative 
+                                    worker with talents in leadership and project management. Terrapin excels in 
+                                    efficiently completing tasks with great attention to detail, organizational skill, 
+                                    and the ability to multitask. Terrapin also has a year of experience in JavaScript,
+                                    is proficient in MongoDB, Express, ReactJS, and Node.js, and is an alumni of the 
+                                    UCLA Extension Fullstack Coding Bootcamp.
                                 </p>
                                 <p> 
-                                    During his time in UCLA Extension's Fullstack Coding Bootcamp,
-
-
-
-
-
-                                    
+                                    During his time in UCLA Extension's Fullstack Coding Bootcamp, Terrapin worked on a variety
+                                    of assignments and <a href="/portfolio" className="roboto">projects</a> that allowed him to hone his exsisting JavaScript skills as well as learn
+                                    a multitude of useful new web development techniques.
                                </p>
-
+                                <div className="space"></div>
                             <div>
                                 <h3>Technical Skills</h3>
                                 <ul>
@@ -51,24 +51,16 @@ function About() {
                             </div>
                                 
                                 <article className="row mt-5">
-                                <article className="col-sm-9 col-xs-12">
-                                    <h3>Dreams</h3>
-                                    <p>
-                                        Terrapin's dream is to start his own video game development company.
-                                        He plays a wide variety of video games to study different genres of gaming
-                                        and currently has a few small projects in the very early stages of deveopment.
-                                    </p>
-                                </article>
-                                </article>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis explicabo sed porro odio quas, corrupti aspernatur quo voluptatum error accusantium modi ipsum iste qui quos excepturi, cumque quod vero alias eligendi veritatis. Nihil reiciendis dolores, nesciunt libero sapiente corrupti sit accusamus. Excepturi aspernatur cupiditate nisi qui ratione doloribus nobis, exercitationem quo dolorem neque repellat voluptatibus! Ratione tempora soluta tempore qui totam ducimus, non facere. Labore asperiores dolor ullam ad sint nobis, quo expedita dolores molestiae ab aliquid totam unde eveniet quis cumque vero consequuntur illum aspernatur assumenda. Unde, perspiciatis aut sapiente ipsa esse dicta eaque similique, totam maiores, neque sequi!
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet culpa doloribus magnam voluptatibus aliquid numquam atque itaque animi ratione voluptas, ut aspernatur velit vitae corporis ducimus vero distinctio eaque tempora quae porro hic iusto et praesentium obcaecati! Autem, temporibus assumenda. Nemo dolor, nam sit consequuntur eum hic perferendis labore, est corporis officiis maiores, reprehenderit corrupti magnam enim ratione. Consectetur mollitia a vitae fuga autem. Nulla, magni unde minima sint vitae, nihil porro quia ipsa alias sapiente officiis. Voluptates officiis, totam magni quas assumenda quidem blanditiis. Libero, fuga placeat. Aliquid amet facilis beatae dolore eius? Cumque reiciendis delectus fugit deserunt exercitationem!
-                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam suscipit necessitatibus qui voluptate, tempore soluta. Tempora cum ex maiores, accusantium veniam quibusdam ut ad magni voluptas quod vitae consequatur ea quo. Ratione dolor assumenda obcaecati animi minus, ipsa autem asperiores perspiciatis incidunt culpa perferendis, doloremque facilis provident aspernatur dicta, tempore eligendi reiciendis ducimus possimus vitae ad. Nisi totam, et repudiandae officiis debitis iure, odit ullam nam nostrum architecto ea earum, quo fugit consequuntur ab voluptatum veniam soluta rerum exercitationem facere fugiat perspiciatis rem numquam. Voluptate ea ipsa repellat praesentium quaerat cumque, autem, corrupti cum similique asperiores nesciunt consectetur doloribus obcaecati?
-                                </p>
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore quae voluptas quia beatae vitae, quas ipsum vel fugit quaerat magnam facere eum quisquam autem culpa minima libero obcaecati voluptates dolor excepturi temporibus explicabo. Hic aut eaque expedita aperiam nam fugiat magnam esse id, quibusdam itaque totam obcaecati molestias officiis ut! Ea impedit culpa veniam pariatur, sapiente error earum corporis totam neque commodi dolorum maiores. Consequatur autem eum accusamus, officia eaque dolor aperiam error nam possimus deleniti nobis mollitia? Facilis quas, dolores doloribus atque inventore amet sequi commodi sit eveniet veritatis excepturi cupiditate laboriosam velit aspernatur provident adipisci alias architecto voluptatibus.
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus, quae facere in molestias repellat officia! Nisi neque omnis eius sequi aut, in qui beatae cumque atque, laborum dolorem libero? Pariatur asperiores, magnam magni blanditiis nostrum ipsa quidem incidunt quia corrupti cumque accusamus debitis modi facere aut laudantium inventore iusto iste sequi ad velit praesentium ex, sit ea cupiditate. Nemo mollitia dolor, ipsum distinctio, at a similique officia repudiandae id dolore, obcaecati iste laborum dolorem. Pariatur non amet, a ipsam officia qui perferendis, reiciendis quo sequi et eligendi magnam quibusdam praesentium inventore nemo expedita iste earum. Blanditiis ullam aperiam quae officia.
-                                </p>    
+                                    <article className="col-sm-9 col-xs-12">
+                                        <h3>Dreams</h3>
+                                        <p>
+                                            Terrapin's dream is to start his own video game development company.
+                                            He plays a wide variety of video games to study different genres of gaming
+                                            and currently has a few small projects in the very early stages of deveopment.
+                                        </p>
+                                    </article>
+                                </article>   
+                                <div className="space"></div>
                             </div>
                         </div>
                     </div>
