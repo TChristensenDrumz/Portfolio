ba28a29fd142b7af1dab7faa6ed3028edd1f7a7b
TChristensenDrumz <tchristensendrumz@gmail.com>
2021-02-13 16:17:55 -0800

    added download resume link

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,11 +3,12 @@ import React from "react";
 function Image() {
   return (
     <div className="float-left pr-3">
-      <img
+      <div><img
         src="https://ca.slack-edge.com/T01C8L6GK9Q-U01CWJ19YBT-f614b873e04d-512"
         className="img-fluid about"
         alt="terrapinchristensen"
-      />
+      /></div>
+      <div className="pt-5 text-center"><a className="contact-link" href="https://drive.google.com/uc?id=1W3Be9OTF7-eNMKt1TvDaFjmPh-FFDP2j&export=download" style={{ textDecoration: "underline" }}>Download Resume</a></div>
     </div>
   );
 }
