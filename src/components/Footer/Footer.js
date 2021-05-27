bc3e09364f9d35178903f5dc681e0b0ea6d274b5
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-05-27 15:38:23 -0700

    google font link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className="footer mt-auto py-3">
             <div className="container-fluid">
-                <span className="text-muted">Email: tchristensendrumz@gmail.com <br/> Phone: (818) 641-2062 <br/> ©2020-2021</span>
+                <span className="roboto text-muted">Email: tchristensendrumz@gmail.com <br/> Phone: (818) 641-2062 <br/> ©2020-2021</span>
             </div>
         </footer>
     );
