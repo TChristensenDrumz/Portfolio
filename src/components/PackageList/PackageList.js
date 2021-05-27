dd30c21f0e8e8ffad53ffd18cb4b70809f5c8922
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-05-27 15:35:49 -0700

    more styling, copyright, packages

--- src/components/PackageList/PackageList.js
+++ src/components/PackageList/PackageList.js
@@ -4,6 +4,12 @@ import "./PackageList.css"
 function PackageList() {
   return <div>
       <h3 className="package-title text-center">Packages</h3>
+      <p className="skill text-center">JSON Web Token (JWT)</p>
+      <p className="skill text-center">Passport</p>
+      <p className="skill text-center">Mongoose</p>
+      <p className="skill text-center">Sequelize</p>
+      <p className="skill text-center">React PDF</p>
+      <p className="skill text-center">EmailJS</p>
   </div>;
 }
 
