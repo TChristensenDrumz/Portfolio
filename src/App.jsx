f7ccb8b04fb69e882dc47c57a023c9eb32e61945
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-11-05 17:10:27 -0400

    new layout

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Styling
+import "./styles/main.css";
+import "./styles/variables.css";
+
+function App() {
+  return (
+    <Router>
+      <Switch></Switch>
+    </Router>
+  );
+}
+
+export default App;
