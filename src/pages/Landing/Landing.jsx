f4fcafa5eddf3f517863bbb2b7e4196c533c303d
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-11-05 18:11:58 -0400

    basic landing layout

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+
+// Style
+import "./Landing.css";
 
 function Landing() {
-  return <div></div>;
+  return (
+    <>
+      <Helmet>
+        <title>Terrapin Christensen</title>
+      </Helmet>
+      <div className="landing-grid">
+        <div className="landing-greeting">
+          <div>Hi, I'm</div>
+          <div>Terrapin Christensen</div>
+          <div>What are you looking for?</div>
+        </div>
+        <div className="landing-web">
+          <div>Web Developer</div>
+        </div>
+        <div className="landing-game">
+          <div>Game Developer</div>
+        </div>
+        <div className="landing-product">
+          <div>Product Manager</div>
+        </div>
+        <div className="landing-music">
+          <div>Musician</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Landing;
