5be3fc650a57d183bbdb980912c703977fb131f2
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-11-05 17:21:03 -0400

    Landing stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+// Pages
+import Landing from "./pages/Landing/Landing";
+import NotFound from "./pages/NotFound/NotFound";
+
 // Styling
 import "./styles/main.css";
 import "./styles/variables.css";
@@ -8,7 +12,9 @@ import "./styles/variables.css";
 function App() {
   return (
     <Router>
-      <Switch></Switch>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+      </Switch>
     </Router>
   );
 }
