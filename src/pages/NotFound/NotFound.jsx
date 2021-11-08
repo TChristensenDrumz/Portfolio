a2474488574be086e9185ba92cb331abac938cbb
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-11-08 16:20:08 -0500

    complete 404 page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { useHistory } from "react-router";
 
 // Styling
 import "./NotFound.css";
 
 function NotFound() {
+  const history = useHistory();
+
   return (
     <>
       <Helmet>
         <title>Page Not Found</title>
       </Helmet>
       <div className="not-found">
-        <i class="far fa-frown"></i>
+        <i class="far fa-frown nf-icon"></i>
+        <div className="nf-header">404</div>
+        <div className="nf-text">Page Not Found</div>
+        <button className="nf-button" onClick={() => history.push("/")}>
+          Return to Home
+        </button>
       </div>
     </>
   );
