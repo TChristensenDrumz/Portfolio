465751dd1f53db4a56ef3a418a791e94f314fa3d
Terrapin Christensen <terrapinchristensen@gmail.com>
2021-11-10 15:41:34 -0500

    placeholder pages

--- src/pages/Musician/Musician.jsx
+++ src/pages/Musician/Musician.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { useHistory } from "react-router";
 
 // Layout
 import Layout from "../../layout/Layout";
@@ -8,12 +9,24 @@ import Layout from "../../layout/Layout";
 import "./Musician.css";
 
 function Musician() {
+  const history = useHistory();
+
   return (
     <>
       <Helmet>
         <title>Terrapin Christensen | Musician</title>
       </Helmet>
-      <Layout type="Music"></Layout>
+      {/* <Layout type="Music"></Layout> */}
+      <div className="music">
+        <i class="fas fa-headphones m-icon"></i>
+        <div className="m-header">Recording in progress</div>
+        {/* <div className="m-header">in progress</div> */}
+        <div className="m-text">This page will be available when</div>
+        <div className="m-text m-pb">all musical assets are ready</div>
+        <button className="m-button" onClick={() => history.push("/")}>
+          Return to Home
+        </button>
+      </div>
     </>
   );
 }
--- src/pages/ProductManager/ProductManager.jsx
+++ src/pages/ProductManager/ProductManager.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { useHistory } from "react-router";
 
 // Layout
 import Layout from "../../layout/Layout";
@@ -8,12 +9,23 @@ import Layout from "../../layout/Layout";
 import "./ProductManager.css";
 
 function ProductManager() {
+  const history = useHistory();
+
   return (
     <>
       <Helmet>
         <title>Terrapin Christensen | Product Manager</title>
       </Helmet>
-      <Layout type="Product"></Layout>
+      {/* <Layout type="Product"></Layout> */}
+      <div className="product-manager">
+        <i class="fas fa-hard-hat pm-icon"></i>
+        <div className="pm-header">This page is</div>
+        <div className="pm-header pm-pb">under construction</div>
+        <div className="pm-text">Please try again later</div>
+        <button className="pm-button" onClick={() => history.push("/")}>
+          Return to Home
+        </button>
+      </div>
     </>
   );
 }
